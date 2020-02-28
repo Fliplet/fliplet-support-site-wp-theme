a3b335918a08529fedffb6395055178543a10b26
Hugo Carneiro <hello@thegeekdeveloper.com>
2020-02-28 19:36:40 +0000

    Tweaks to 404 page

--- 404.php
+++ 404.php
@@ -1,5 +1,14 @@
 <?php get_header();?>
 
-<h3>Oops! Page can&rsquo;t be found.</h3>
+<div class="error-404 not-found">
+  <header class="page-header">
+    <h1 class="page-title"><?php _e( 'Oops! The page can&rsquo;t be found.', 'flipletsupport' ); ?></h1>
+  </header>
+
+  <div class="page-content">
+    <p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'flipletsupport' ); ?></p>
+    <?php get_search_form(); ?>
+  </div>
+</div>
 
 <?php get_footer();?>
