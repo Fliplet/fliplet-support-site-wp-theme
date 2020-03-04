6f474cc8d0d884c25c32f3093a80247c4b184949
Hugo Carneiro <hello@thegeekdeveloper.com>
2020-03-04 16:10:17 +0000

    Fix category selection

--- template-parts/content/content-categories.php
+++ template-parts/content/content-categories.php
@@ -9,13 +9,21 @@
 
 ?>
 
+<?php
+$queriedObject = get_queried_object();
+$currentCategoryId = $queriedObject->term_id;
+$postCategoryArray = get_the_category(false);
+$foundCategory = current(array_filter($postCategoryArray, function($e) use($currentCategoryId) { return $e->term_id == 4; }));
+$isCurrentCategory = $foundCategory;
+?>
+
 <ul class="category-list">
   <li class="cat-item <?php echo is_category() ? '' : 'current-cat' ?>">
     <a aria-current="page" href="<?php echo get_permalink( get_option( 'page_for_posts' ) ); ?>">All</a>
   </li>
   <?php wp_list_categories(array(
     'hide_empty' => 0,
-    'current_category' => is_category() ? get_the_category(false) : 0,
+    'current_category' => $isCurrentCategory,
     'title_li' => ''
   )); ?> 
 </ul>
