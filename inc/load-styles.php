eb184bfb4b8f84bc90ab40d12568c35bf5a66e68
Hugo Carneiro <hello@thegeekdeveloper.com>
2020-03-04 16:09:08 +0000

    Added Fliplet Icons font

--- inc/load-styles.php
+++ inc/load-styles.php
@@ -14,6 +14,12 @@ function load_bootstrap_css() {
   wp_enqueue_style('bootstrap_css');
 }
 
+function load_fliplet_fonts() {
+  wp_register_style('fliplet_fonts', get_template_directory_uri() . '/fonts/fliplet-icons/style.css', array(), '1.0.0', 'all');
+
+  wp_enqueue_style('fliplet_fonts');
+}
+
 function load_theme_styles() {
   wp_register_style('theme_style', get_template_directory_uri() . '/style.css', array(), '1.0.0', 'all');
 
@@ -21,5 +27,6 @@ function load_theme_styles() {
 }
 
 add_action('wp_enqueue_scripts', 'load_bootstrap_css');
+add_action('wp_enqueue_scripts', 'load_fliplet_fonts');
 add_action('wp_enqueue_scripts', 'load_theme_styles');
 ?>
