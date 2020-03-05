97fad219e4f0983bce4052f9b0e0b30b31e9abed
Hugo Carneiro <hello@thegeekdeveloper.com>
2020-03-05 11:37:24 +0000

    Remove IDs from search form

--- template-parts/search/search-form-autofocus.php
+++ template-parts/search/search-form-autofocus.php
@@ -10,10 +10,10 @@
  */
 ?>
 
-<form role="search" method="get" id="searchform" class="searchform" action="<?php echo get_home_url(); ?>">
+<form role="search" method="get" class="searchform" action="<?php echo get_home_url(); ?>">
   <div class="search-input-group">
-    <input class="form-control" type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" placeholder="Search..." autofocus>
-    <button type="submit" id="searchsubmit" value="Search">
+    <input class="form-control" type="text" value="<?php echo get_search_query(); ?>" name="s" placeholder="Search..." autofocus>
+    <button type="submit" value="Search">
       <i class="fl-icon-search"></i>
     </button>
   </div>
--- template-parts/search/search-form.php
+++ template-parts/search/search-form.php
@@ -10,10 +10,10 @@
  */
 ?>
 
-<form role="search" method="get" id="searchform" class="searchform" action="<?php echo get_home_url(); ?>">
+<form role="search" method="get" class="searchform" action="<?php echo get_home_url(); ?>">
   <div class="search-input-group">
-    <input class="form-control" type="text" value="" name="s" id="s" placeholder="Search...">
-    <button type="submit" id="searchsubmit" value="Search">
+    <input class="form-control" type="text" value="" name="s" placeholder="Search...">
+    <button type="submit" value="Search">
       <i class="fl-icon-search"></i>
     </button>
   </div>
