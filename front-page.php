8bc0c93ded3b9812ff35ea2299c02c888b3537c8
Hugo Carneiro <hello@thegeekdeveloper.com>
2020-03-05 16:23:13 +0000

    Front page render content

--- front-page.php
+++ front-page.php
@@ -15,13 +15,17 @@
 get_header();
 ?>
 
-<?php
-if (have_posts()):
-  while (have_posts()):
-    the_post();
-    get_template_part('template-parts/content/content', 'page');
-  endwhile;
-endif;
-?>
+<article class="container-lg">
+
+  <?php
+  if (have_posts()):
+    while (have_posts()):
+      the_post();
+      the_content();
+    endwhile;
+  endif;
+  ?>
+
+</article>
 
 <?php get_footer();?>
