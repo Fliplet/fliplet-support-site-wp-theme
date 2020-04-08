aa530ee76d78ddd1c2d73e09cde0a2da1ce2e13a
Hugo Carneiro <hello@thegeekdeveloper.com>
2020-04-08 12:41:32 +0100

    New menu

--- template-parts/content/article-header.php
+++ template-parts/content/article-header.php
@@ -13,4 +13,12 @@
 <div class="article-header">
   <h1><?php echo get_the_title( get_option('page_for_posts', true) ); ?></h1>
   <p>You can extend your app by adding in custom code. We've created a reference library of examples to demo common use cases.</p>
+  <?php
+  wp_nav_menu(
+    array(
+      'theme_location' => 'code-library-menu',
+      'menu_class' => 'code-library-menu'
+    )
+  );
+  ?>
 </div>
