2a942cf022c5f7c6f96092075cce3f8f3f042f7f
Hugo Carneiro <hello@thegeekdeveloper.com>
2020-06-10 13:41:31 +0100

    Intercom integration

--- footer.php
+++ footer.php
@@ -65,5 +65,19 @@
 
   <?php wp_footer(); ?>
 
+  <script>
+    window.intercomSettings = {
+      app_id: "eo99qllm",
+      horizontal_padding: 20,
+      vertical_padding: 20,
+      hide_default_launcher: false
+    };
+  </script>
+
+  <script>
+  // We pre-filled your app ID in the widget URL: 'https://widget.intercom.io/widget/eo99qllm'
+  (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/eo99qllm';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+  </script>
+
 </body>
 </html>
